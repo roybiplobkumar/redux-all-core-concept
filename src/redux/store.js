f501d8cea3ec7725fcf300407a33ec28666e9950
roy-biplobe <biplobkumargk@gmail.com>
2025-01-31 07:56:19 +0600

    finish

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import couterReduser from '../redux/features/counter/counterSlice'
+import logger from 'redux-logger'
 
 const store=configureStore({
-  reducer:{couterReduser}
+  reducer:{couterReduser},
+  middleware :(getDefaultMiddleware)=>{
+   return getDefaultMiddleware().concat(logger)
+  }
 })
 export default store;
